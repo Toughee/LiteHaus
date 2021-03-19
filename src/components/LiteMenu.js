df904aaa25b20cf357f611beb97bdcfd513f6195
Toughee <Jimernez.io@gmail.com>
2021-03-19 11:18:19 -0500

    LiteHaus/015/Code_Refactoring_002 - Updated components and node modules, fixing current bug involving 3 security vulnerabilities - Finalized app and U/I components. Bundled app for alpha release

--- src/components/LiteMenu.js
+++ src/components/LiteMenu.js
@@ -1,125 +1,139 @@
-import React, {useState} from 'react';
+import React from 'react';
 import '../css/LiteMenu.css';
 
-function LiteMenu(LiteSetting) {
-	const LiteStatus = LiteSetting ? 'On' : 'Off';
+function LiteMenu(value) {
+	const LiteStatus = value ? 'On' : 'Off';
+	return <span className={LiteStatus}>{LiteStatus}</span>;
+}
 
-	// class LiteSwitches extends React.Component { 
-	// 	state = {
-	// 		garageDoor: true,
-	// 		backyard: false,
-	// 		frontDoor: true,
-	// 		livingRoom: true,
-	// 		kitchen: false,
-	// 		bathroom: false,
-	// 		masterSuite: true,
-	// 		suiteBathroom: false,
-	// 		guestRoom: false
-	// 	};
+class LiteSwitches extends React.Component { 
+	state = {
+		garageDoor: true,
+		backyard: false,
+		frontDoor: true,
+		livingRoom: true,
+		kitchen: false,
+		bathroom: false,
+		masterSuite: true,
+		suiteBathroom: false,
+		guestRoom: false
+	};
 
-	// 	toggle = room => {
-	// 		this.setState(prevState => ({
-	// 		[room]: !prevState[room]
-	// 		}));
-	// 	}; 
+	toggle = room => {
+		this.setState(prevState => ({
+		[room]: !prevState[room]
+		}));
+	}; 
 
-	// render() {
-	// 	const {
-	// 		garageDoor,
-	//   		backyard,
-	//   		frontDoor,
-	//   		livingRoom,
-	//   		kitchen,
-	//   		bathroom,
-	//   		masterSuite,
-	//   		suiteBathroom,
-	//   		guestRoom
-	// 	} = this.state;
+		render() {
+			const {
+				garageDoor,
+		  		backyard,
+		  		frontDoor,
+		  		livingRoom,
+		  		kitchen,
+		  		bathroom,
+		  		masterSuite,
+		  		suiteBathroom,
+		  		guestRoom
+		} = this.state;
 
 
-return (
-		<div className="Litemenu-bar">
+	return (
+	<div className="Litemenu-bar">
+
+		<h1>LiteHaus</h1>
+		<span className="Lite-panel-label"> 
+			<i className='fas fa-warehouse'></i>
+			Garage Door : 
+			<button onClick={() => this.toggle('garageDoor')}> 
+			{LiteMenu(garageDoor)}
+		</button>
+		</span>
+
+		<br/>
 		
-			<h1>LiteHaus</h1>
-			
-			<span className="Lite-panel-label"> 
-				<i className='fas fa-warehouse'></i>
-				  Garage door: 
-				<button onClick = {() => this.toggle('garageDoor')}>
-				On{/*LiteStatus(garageDoor)*/}</button>
-			</span>
-			<br />
-			
-			<span className="Lite-panel-label">
-				<i className='fas fa-campground'></i>
-				  Backyard: 
-				<button onClick = {() => this.toggle('backyard')}>
-				Off {/*LiteStatus(backyard)*/}</button>
-			</span>
-			<br />
+		<span className="Lite-panel-label"> 
+			<i className='fas fa-campground'></i>
+			Backyard : 
+			<button onClick={() => this.toggle('backyard')}> 
+			{LiteMenu(backyard)}
+		</button>
+		</span>
 
-			<span className="Lite-panel-label">
-				<i className='fas fa-door-closed'></i>
-				  Front Door:  
-				<button onClick = {() => this.toggle('frontDoor')}>
-				On {/*LiteStatus(frontDoor)*/}</button>
-			</span>
+		<br/>
+		
+		<span className="Lite-panel-label"> 
+			<i className='fas fa-door-closed'></i>
+			Front Door : 
+			<button onClick={() => this.toggle('frontDoor')}> 
+			{LiteMenu(frontDoor)}
+		</button>
+		</span>
 
-			<br />
-			
-			<span className="Lite-panel-label">
-				<i className='fas fa-television'></i>
-				  Living room:   
-				<button onClick = {() => this.toggle('livingRoom')}>
-				On {/*LiteStatus(livingRoom:)*/}</button>
-			</span>
-			<br />
-			
-			<span className="Lite-panel-label">
-				<i className='fas fa-cookie'></i>
-				 Kitchen:  
-				<button onClick = {() => this.toggle('kitchen')}>
-				Off {/*LiteStatus(kitchen)*/}</button>
-			</span>
-			
-			<br />
+		<br/>
+		
+		<span className="Lite-panel-label"> 
+			<i className='fas fa-television'></i>
+			Living room : 
+			<button onClick={() => this.toggle('livingRoom')}> 
+			{LiteMenu(livingRoom)}
+		</button>
+		</span>
 
-			<span className="Lite-panel-label">
-				<i className='fas fa-bath'></i>
-				 Bathroom: 
-				<button onClick = {() => this.toggle('bathroom')}>
-				Off {/*LiteStatus(bathroom)*/}</button>
-			</span>
-			
-			<br />
-			<span className="Lite-panel-label">
-				<i className='fas fa-bed'></i>
-				 Master Suite:
-				<button onClick = {() => this.toggle('masterSuite')}>
-				Off {/*LiteStatus(masterSuite)*/} </button>
-			</span>
+		<br/>
 
-			<br />
-			
-			<span className="Lite-panel-label">
-				<i className='fas fa-bath'></i>
-				 Suite Bathroom: 
-				<button onClick = {() => this.toggle('suiteBathroom')}>
-				Off {/*LiteStatus(suiteBathroom)*/}</button>
-			</span>
+		<span className="Lite-panel-label"> 
+			<i className='fas fa-cookie'></i>
+			Kitchen: 
+			<button onClick={() => this.toggle('kitchen')}> 
+			{LiteMenu(kitchen)}
+		</button>
+		</span>
 
-			<br />
+		<br/>
+
+		<span className="Lite-panel-label"> 
+			<i className='fas fa-bath'></i>
+			Bathroom: 
+			<button onClick={() => this.toggle('bathroom')}> 
+			{LiteMenu(bathroom)}
+		</button>
+		</span>
+
+		<br />
+
+		<span className="Lite-panel-label"> 
+			<i className='fas fa-bed'></i>
+			Master Bedroom: 
+			<button onClick={() => this.toggle('masterSuite')}> 
+			{LiteMenu(masterSuite)}
+		</button>
+		</span>
+
+		<br />
+
+		<span className="Lite-panel-label"> 
+			<i className='fas fa-bed'></i>
+			Suite Bathroom: 
+			<button onClick={() => this.toggle('suiteBathroom')}> 
+			{LiteMenu(suiteBathroom)}
+		</button>
+		</span>
+
+		<br />
+
+		<span className="Lite-panel-label"> 
+			<i className='fas fa-bed'></i>
+			Guest Room: 
+			<button onClick={() => this.toggle('guestRoom')}> 
+			{LiteMenu(guestRoom)}
+		</button>
+		</span>
 			
-			<span className="Lite-panel-label">
-				<i className='fas fa-bed'></i>
-				 Guest Room: 
-				<button onClick = {() => this.toggle('guestRoom')}>
-				Off {/*LiteStatus(guestRoom)*/}  </button>
-			</span>
-			<br />
-		</div>
+	</div>
+
 		);
 	}
-
-
-export default LiteMenu;
+}
+export default LiteSwitches;
